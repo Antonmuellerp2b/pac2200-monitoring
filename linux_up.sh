3ab8b2ce723a4d049213415af35b1d75a4c75af6
Antonmuellerp2b <anton.mueller@power2bit.com>
2025-06-16 11:45:30 +0200

    Updated linux_up.sh

--- linux_up.sh
+++ linux_up.sh
@@ -6,3 +6,7 @@ set -e
 
 # Start docker compose with any arguments passed to this script
 exec docker-compose up "$@"
+
+# Change ownership of the grafana data directory to UID 472 (Grafana user)
+# to avoid permission issues when running the Grafana Docker container
+sudo chown -R 472:472 ./grafana
