018da9c5002caf50618f481fd0df8c39df13a261
Antonmuellerp2b <anton.mueller@power2bit.com>
2025-07-30 16:13:48 +0200

    Fixed linux version and added multiple default e-mails in default.env

--- linux_up.sh
+++ linux_up.sh
@@ -7,6 +7,9 @@ set -e
 # Render influxdb.yaml from template using .env
 ./render_influxdb_yaml.sh
 
+# Render contact-points.yaml from template using .env
+./render_contact_points_yaml.sh
+
 # Set permissions for Grafana data directory
 [ "$(uname)" = "Linux" ] && sudo chown -R 472:472 ./grafana || true
 
--- render_influxdb_yaml.sh
+++ render_influxdb_yaml.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Load .env variables from project root
