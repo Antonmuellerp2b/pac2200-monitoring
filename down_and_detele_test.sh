df911d7e468c64b553378924a411235be49a25d3
Antonmuellerp2b <anton.mueller@power2bit.com>
2025-09-10 11:41:33 +0200

    Removed scripts for testing

--- down_and_detele_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-set -e
-
-# Stoppe alle Container des temporären Test-Projekts und lösche auch die Volumes
-docker compose -p test_project down -v
-
-echo "✅ Temporäres Testprojekt gestoppt und Volumes gelöscht."
