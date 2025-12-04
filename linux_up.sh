2a8c945816d7944f3a3fc5171a1fac5979649c26
Antonmuellerp2b <anton.mueller@power2bit.com>
2025-12-04 16:11:52 +0100

    Removed linux permission change in linux_up.sh

--- linux_up.sh
+++ linux_up.sh
@@ -5,24 +5,12 @@
 
 set -eu
 
-# Set permissions for Grafana directory to current user
-if [ "$(uname)" = "Linux" ]; then
-    echo "Setting Grafana directories ownership to $USER"
-    chown -R "$USER":"$USER" ./grafana
-fi
-
 # Render all required YAML files from templates
 ./render_influxdb_yaml.sh
 ./render_contact_points_yaml.sh
 ./render_power_imbalance_rule.sh
 ./render_power_sum_max_rule.sh
 ./render_telegraf.conf.template.sh
-
-# Make Grafana directories writable by everyone
-if [ "$(uname)" = "Linux" ]; then
-    echo "Setting Grafana directories writable by all users"
-    chmod -R a+rwX ./grafana
-fi
 
 # Start docker compose
 exec docker compose up "$@"
